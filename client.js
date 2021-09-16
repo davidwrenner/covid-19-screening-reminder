9adad0acaf5457aeff57030767110ef18de38dfb
David Wrenner <davidwrenner@wustl.edu>
2021-09-16 12:30:04 -0500

    user input assist

--- client.js
+++ client.js
@@ -34,18 +34,45 @@ function submitRegistration() {
                 "<a href='https://www.linkedin.com/in/davidwrenner'><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-linkedin\" viewBox=\"0 0 16 16\">\n" +
                 "  <path d=\"M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z\"/>\n" +
                 "</svg> Connect with me<a>";
-
-
         }
         else{
             alert("Unable to register with the provided information.");
         }
     })
     .catch(err => console.error(err));
-
 }
 
+// function to help user enter the correct input format
+function assistPhoneInput(e) {
+    let element = document.getElementById("phone-number");
+    let len = element.value.length;
+
+    // remove parentheses if added
+    if (element.value.charAt(len - 1) == "(" || element.value.charAt(len - 1) == ")") {
+        element.value = element.value.substring(0, len - 1);
+    }
+    // auto fill "-" in phone number so user doesn't have to
+    else if (len == 3 || len == 7) {
+        // but only fill in if the user is not trying to delete their entry
+        if(e.code != "Backspace") {
+            element.value += "-";
+        }
+    }
+    // if user enters an additional "-" after autofill remove it
+    else if (len == 5 || len == 9) {
+        if (element.value.charAt(4) == "-" || element.value.charAt(8) == "-") {
+            element.value = element.value.substring(0, len - 1);
+        }
+    }
+    // if too many digits are entered, remove the spare one
+    else if (len == 13) {
+        element.value = element.value.substring(0, len - 1);
+    }
+
+}
 document.getElementById("submit-reg-btn").addEventListener("click", function(e){
     e.preventDefault();
     submitRegistration();
 });
+
+document.getElementById("phone-number").addEventListener("keyup", assistPhoneInput);
